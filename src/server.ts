aee772ec06002cc5a364bf4bac634825f8be3bc9
ThabCode <abmael_ninha@hotmail.com>
2022-11-07 06:39:56 -0400

    Adicionando documentação

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
-import { app } from ".";
+/* eslint-disable prettier/prettier */
+import swaggerUi from "swagger-ui-express";
 
+import { app } from ".";
+import swaggeFile from "./swagger.json"
+
+
+app.use("/ip-docs", swaggerUi.serve, swaggerUi.setup(swaggeFile));
 app.listen(3333, () => console.log("Server is running!"));
+
